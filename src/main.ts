5bcc9995f97b6114edc4c44263421a5fb6b07f6e
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-14 16:09:00 -0400

    feat: add token validation to routes

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,26 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 import './assets/main.css'
+import { authStore } from './store'
 
 const SignIn = () => import('./components/SignIn.vue')
 const HomePage = () => import('./components/HomePage.vue')
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: SignIn },
-  { path: '/home', component: HomePage }
+  { path: '/login', component: SignIn, name: 'login' },
+  { path: '/home', component: HomePage, name: 'home' }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach(async (to) => {
+  if (!authStore.token && to.name !== 'login') {
+    return { name: 'login' }
+  }
 })
 
 const app = createApp(App)
