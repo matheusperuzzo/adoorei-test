eefc6d5b49afe308d4f49f0c22ce8ff1e2f5da55
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-14 12:11:26 -0400

    feat: add desired colors and extra spacing values

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    colors: {
+      black: '#292d32',
+      rose: '#F30168',
+      slate: {
+        50: '#f8fafc',
+        100: '#f1f5f9',
+        200: '#e2e8f0',
+        300: '#cbd5e1',
+        400: '#94a3b8',
+        500: '#64748b',
+        600: '#475569',
+        700: '#334155',
+        800: '#1e293b',
+        900: '#0f172a',
+        950: '#020617'
+      },
+      white: '#FFFFFF'
+    },
+    extend: {
+      spacing: {
+        1.25: '0.313rem',
+        3.75: '0.938rem',
+        6.25: '1.563rem',
+        7.5: '1.875rem',
+        47.5: '11.875rem'
+      }
+    }
   },
   plugins: []
 }
