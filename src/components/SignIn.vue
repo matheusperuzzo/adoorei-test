3a752b81f904c6f6c9c263b5ec66185ee45a64d4
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-14 16:09:20 -0400

    feat: add authenticate method

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,16 +1,53 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+import { authStore, userStore } from '@/store'
 import LocawebLogo from './icons/LocawebLogo.vue'
+
+import type { LoginBody } from '@/store'
+import { useRouter } from 'vue-router'
+
+const credentials = ref<LoginBody>({
+  email: '',
+  password: ''
+})
+
+const error = ref('')
+
+const router = useRouter()
+
+const authenticate = async (event: Event) => {
+  event.preventDefault()
+
+  const response = await authStore.login(credentials.value)
+  if (response.code !== 200) {
+    error.value = response.body.message
+    return
+  }
+
+  router.push({
+    path: 'home'
+  })
+}
+
+onMounted(() => {
+  userStore.getAll()
+})
 </script>
 
 <template>
-  <section class="h-2/3 mx-auto py-47.5 w-5/12">
+  <section class="h-2/3 mx-auto pt-47.5 w-5/12">
     <LocawebLogo class="mb-8 mx-auto" />
-    <form class="bg-white border border-slate-200 mb-8 px-8 py-8 rounded-md w-full">
+    <form
+      @submit="authenticate"
+      class="bg-white border border-slate-200 mb-8 px-8 py-8 rounded-md w-full"
+    >
       <h2 class="font-bold mb-1.25 text-3xl">Entre na sua conta</h2>
       <p class="leading-snug text-lg mb-5">Para acessar sua conta informe seu e-mail e senha</p>
       <div class="mb-3.75 w-full">
         <label class="block leading-5" for="email">E-mail</label>
         <input
+          v-model="credentials.email"
           class="border border-slate-700/50 pl-4 py-7.5 rounded-md placeholder:text-black w-full"
           type="email"
           id="email"
@@ -20,6 +57,7 @@ import LocawebLogo from './icons/LocawebLogo.vue'
       <div class="mb-6.25 w-full">
         <label class="block leading-5" for="password">Senha</label>
         <input
+          v-model="credentials.password"
           class="border border-slate-700/50 mb-1.25 pl-4 py-7.5 rounded-md placeholder:text-black w-full"
           type="password"
           id="password"
