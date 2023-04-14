0903f6b9077ae27f8885c841770494f0f7439add
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-14 12:42:33 -0400

    feat: add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const SignIn = () => import('./components/SignIn.vue')
+
+const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: SignIn }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
