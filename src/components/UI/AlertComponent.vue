a3006e3c2a7cb51c5b7b0ac8b198c06555f2c19a
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-15 18:28:23 -0400

    feat: create Alert component

---
+++ src/components/UI/AlertComponent.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Button from './ButtonComponent.vue'
+import Card from './CardComponent.vue'
+
+type AlertTypes = 'error' | 'success' | 'warn'
+
+interface Emits {
+  (e: 'close'): void
+}
+
+interface Props {
+  message: string
+  type: AlertTypes
+}
+
+defineEmits<Emits>()
+defineProps<Props>()
+
+const classes = {
+  error: 'bg-primary-50 text-primary',
+  success: '',
+  warn: ''
+}
+</script>
+
+<template>
+  <Card :class="'flex items-center justify-between rounded-md ' + classes[type]">
+    <div>
+      <h4 class="font-semibold">Houve um erro!</h4>
+      <p class="text-sm">{{ message }}</p>
+    </div>
+    <Button @click="$emit('close')">X</Button>
+  </Card>
+</template>
