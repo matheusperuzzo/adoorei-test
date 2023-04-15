134f6dbe9299f8d245359358fe198c69b54ede24
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-15 16:02:58 -0400

    feat: add formField validations

--- src/components/UI/FormFieldComponent.vue
+++ src/components/UI/FormFieldComponent.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, reactive } from 'vue'
+
+interface FormValidation {
+  isTouched: boolean
+  isValid: boolean
+  validate: () => void
+}
 
 interface Props {
-  value: any
-  type: string
+  errorMsg: string
   id: string
   placeholder?: string
+  type: string
+  validationFn: (value: any) => boolean
+  value: any
 }
 
 const emit = defineEmits(['update:value'])
 const props = defineProps<Props>()
+
+const formValidation = reactive<FormValidation>({
+  isTouched: false,
+  isValid: false,
+  validate() {
+    this.isTouched = true
+    this.isValid = props.validationFn(value.value)
+  }
+})
 
 const value = computed({
   get() {
@@ -25,12 +42,16 @@ const value = computed({
   <div class="mb-3.75 w-full">
     <label class="block leading-5" :for="id">E-mail</label>
     <input
+      @blur="formValidation.validate"
       class="border border-slate-700/50 pl-4 py-7.5 rounded-md placeholder:text-black w-full"
       :id="id"
       :placeholder="placeholder"
       :type="type"
       v-model="value"
     />
+    <span class="text-rose text-xs" v-if="formValidation.isTouched && !formValidation.isValid">{{
+      errorMsg
+    }}</span>
     <slot></slot>
   </div>
 </template>
