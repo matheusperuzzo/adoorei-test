3020648d657b12e23b03a8ac9d1ed7939a50b35f
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-15 18:28:08 -0400

    feat: create Button component

---
+++ src/components/UI/ButtonComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+  type?: 'submit' | 'reset' | 'button'
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <button :type="type || 'button'">
+    <slot></slot>
+  </button>
+</template>
