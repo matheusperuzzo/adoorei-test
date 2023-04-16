022edcf0ed519698d182e7f2f5b866d42724881d
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-16 00:17:30 -0400

    feat: add checkUser method

--- src/app/store/user.ts
+++ src/app/store/user.ts
@@ -4,11 +4,21 @@ import type { User } from '@/app/shared/models'
 
 type UserStore = {
   users: User[]
+  checkUser: (email: string) => User | null
   getAll: () => Promise<void>
 }
 
 export const userStore = reactive<UserStore>({
   users: [],
+  checkUser(email) {
+    const userExists = this.users.find((user) => user.email === email)
+
+    if (!userExists) {
+      return null
+    }
+
+    return userExists
+  },
   async getAll() {
     const response = await fetch(`${import.meta.env.VITE_API_URL}users`)
 
