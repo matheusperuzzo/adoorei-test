05631bb4ac655d72374da6bc00af215f48ef7fde
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-16 00:18:22 -0400

    refactor: use router from separate file

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,9 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
 
 import './assets/main.css'
 
 import App from './app/App.vue'
-import { authStore } from './app/store'
-
-const HomePage = () => import('./app/pages/Home/HomePage.vue')
-const Login = () => import('./app/pages/Login/LoginPage.vue')
-
-const routes = [
-  { path: '/', redirect: '/login' },
-  { path: '/home', component: HomePage, name: 'home' },
-  { path: '/login', component: Login, name: 'login' }
-]
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
-
-router.beforeEach(async (to) => {
-  if (!authStore.token && to.name !== 'login') {
-    return { name: 'login' }
-  }
-})
+import router from './routing/router'
 
 const app = createApp(App)
 
