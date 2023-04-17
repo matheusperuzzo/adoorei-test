5982b5d3ad7d451f342cc7bbf6a1541d47a641be
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-17 14:04:27 -0400

    fix: adjust card styling

--- src/app/components/UI/CardComponent.vue
+++ src/app/components/UI/CardComponent.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mx-auto rounded-md">
+  <div class="card">
     <slot></slot>
   </div>
 </template>
+
+<style>
+.card {
+  border-radius: 0.375rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
