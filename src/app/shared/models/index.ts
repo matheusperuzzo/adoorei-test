66baf52d2388718c3ddfcdc02439d4e030f53c49
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-17 14:44:32 -0400

    feat: create Step model

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -1,3 +1,4 @@
 export * from './http.model'
 export * from './product.model'
+export * from './step.model'
 export * from './user.model'
---
+++ src/app/shared/models/step.model.ts
@@ -0,0 +1,3 @@
+export interface Step {
+  [key: string | number]: any
+}
