e7198c4d5fb2b8d7bcddd569f58be2b40964f2f0
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-18 12:13:15 -0400

    fix: password validation

--- src/app/shared/constants/validationFunctions.ts
+++ src/app/shared/constants/validationFunctions.ts
@@ -1,4 +1,4 @@
 import { emailRegex } from './regex'
 
 export const emailValidation = (value: string) => emailRegex.test(value)
-export const passwordValidation = (value: string) => value.length >= 6
+export const passwordValidation = (value: string) => value.length >= 8
