7cd2e52366a45ff3d5348d805b86cd67e789843d
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-18 02:21:31 -0400

    feat: expose isChecked property

--- src/app/components/UI/CheckboxComponent.vue
+++ src/app/components/UI/CheckboxComponent.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import CheckIcon from '../assets/icons/CheckIcon.vue'
+
+const isChecked = ref(false)
+
+defineExpose({ isChecked })
 </script>
 
 <template>
-  <label id="container" class="cursor-pointer flex gap-3 items-center">
+  <label
+    id="container"
+    class="cursor-pointer flex gap-3 items-center"
+    @click="isChecked = !isChecked"
+  >
     <div
       id="custom-check"
       class="bg-primary-50 hover:bg-primary-200/50 border border-primary flex h-4 items-center justify-center rounded text-primary min-w-3.5"
     >
-      <CheckIcon width="7" height="8" />
+      <CheckIcon v-if="isChecked" width="7" height="8" />
     </div>
     <slot></slot>
   </label>
