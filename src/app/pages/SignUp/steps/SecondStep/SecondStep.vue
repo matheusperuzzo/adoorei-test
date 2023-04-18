e53b58296873dcb9bcbacfec6a66b4042d195db5
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-18 12:50:32 -0400

    feat: pass errorMsg to Form component

--- src/app/pages/SignUp/steps/SecondStep/SecondStep.vue
+++ src/app/pages/SignUp/steps/SecondStep/SecondStep.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 import ChosenProduct from './ChosenProductComponent.vue'
@@ -14,15 +15,24 @@ interface Props {
   chosenProductId: number
 }
 
+const errorMsg = ref<string>('')
+
 const router = useRouter()
 
 const createUser = async (personalData: any) => {
   await userStore.addUser(personalData)
 
-  await authStore.login({
+  const response = await authStore.login({
     email: 'john@gmail.com',
     password: 'm38rmF$'
   })
+
+  console.log(response)
+
+  if (response.code !== 200) {
+    errorMsg.value = 'Não foi possível realizar a autenticação no momento!'
+    return
+  }
 
   router.push({ path: 'home' })
 }
@@ -35,7 +45,7 @@ defineProps<Props>()
   <section
     class="flex flex-col-reverse lg:flex-row gap-5 items-center lg:items-stretch justify-center mt-12"
   >
-    <Form @create-user="($event) => createUser($event)"></Form>
+    <Form v-model:error="errorMsg" @create-user="($event) => createUser($event)"></Form>
     <ChosenProduct
       @change-product="(_$event) => $emit('changeProduct')"
       :productId="chosenProductId"
