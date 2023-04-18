d42383441d73989e56c74cd827bb349f510643c3
Matheus Peruzzo <matheusperuzzooficial@gmail.com>
2023-04-17 22:36:46 -0400

    feat: create Checkbox component

---
+++ src/app/components/UI/CheckboxComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import CheckIcon from '../assets/icons/CheckIcon.vue'
+</script>
+
+<template>
+  <label id="container" class="cursor-pointer flex gap-3 items-center">
+    <div
+      id="custom-check"
+      class="bg-primary-50 hover:bg-primary-200/50 border border-primary flex h-4 items-center justify-center rounded text-primary min-w-3.5"
+    >
+      <CheckIcon width="7" height="8" />
+    </div>
+    <slot></slot>
+  </label>
+</template>
+
+<style>
+#container:hover #custom-check {
+  background-color: rgb(249 128 180 / 0.5) !important;
+}
+</style>
